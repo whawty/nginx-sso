949e98e0dc261b69179e74418598881d71817816
Christian Pointner <equinox@spreadspace.org>
2023-11-07 18:51:40 +0100

    implemented LDAP backend (WIP)

--- auth/backend_ldap.go
+++ auth/backend_ldap.go
@@ -33,17 +33,19 @@ package auth
 import (
 	"fmt"
 	"log"
-	//	"github.com/go-ldap/ldap/v3"
+	"strings"
+
+	"github.com/go-ldap/ldap/v3"
 )
 
 type LDAPConfig struct {
-	Server            string `yaml:"server"`
-	RootDN            string `yaml:"root_dn"`
-	ManagerDN         string `yaml:"manager_dn"`
-	ManagerPassword   string `yaml:"manager_password"`
-	UserSearchBase    string `yaml:"user_search_base"`
-	UserSearchFilter  string `yaml:"user_search_filter"`
-	UsernameAttribute string `yaml:"username_attribute"`
+	Servers           []string `yaml:"servers"`
+	RootDN            string   `yaml:"root_dn"`
+	ManagerDN         string   `yaml:"manager_dn"`
+	ManagerPassword   string   `yaml:"manager_password"`
+	UserSearchBase    string   `yaml:"user_search_base"`
+	UserSearchFilter  string   `yaml:"user_search_filter"`
+	UsernameAttribute string   `yaml:"username_attribute"`
 	// TODO: TLS
 }
 
@@ -63,13 +65,69 @@ func NewLDAPBackend(conf *LDAPConfig, infoLog, dbgLog *log.Logger) (Backend, err
 	if conf.UsernameAttribute == "" {
 		conf.UsernameAttribute = "uid"
 	}
+	if len(conf.Servers) == 0 {
+		return nil, fmt.Errorf("ldap: at least server must be configured")
+	}
 
 	b := &LDAPBackend{conf: conf, infoLog: infoLog, dbgLog: dbgLog}
 	infoLog.Printf("ldap: successfully intialized")
 	return b, nil
 }
 
-func (w *LDAPBackend) Authenticate(username, password string) error {
-	// TODO: implement this: https://pkg.go.dev/gopkg.in/ldap.v3#example-package-UserAuthentication
-	return fmt.Errorf("not yet implemented")
+func (w *LDAPBackend) authenticate(server, username, password string) (bool, error) {
+	// TODO: add ldap.DialWithTLSConfig(..)
+	l, err := ldap.DialURL(server)
+	if err != nil {
+		return true, err
+	}
+	defer l.Close()
+
+	// TODO: do this if configured
+	// if err = l.StartTLS(&tls.Config{InsecureSkipVerify: true}); err != nil {
+	// 	return true, err
+	// }
+
+	if err = l.Bind(w.conf.ManagerDN, w.conf.ManagerPassword); err != nil {
+		return true, err
+	}
+
+	f := strings.NewReplacer("{0}", ldap.EscapeFilter(username)).Replace(w.conf.UserSearchFilter)
+	a := []string{w.conf.UsernameAttribute}
+	searchRequest := ldap.NewSearchRequest(w.conf.UserSearchBase, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false, f, a, nil)
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		return true, err
+	}
+	if len(sr.Entries) == 0 {
+		return false, fmt.Errorf("user not found")
+	}
+	if len(sr.Entries) > 1 {
+		return false, fmt.Errorf("user search filter returned multiple results")
+	}
+
+	if err = l.Bind(sr.Entries[0].DN, password); err != nil {
+		return false, err
+	}
+	return false, nil
+}
+
+func (w *LDAPBackend) Authenticate(username, password string) (err error) {
+	// make sure we don't trigger this: https://github.com/go-ldap/ldap/issues/93
+	if username == "" || password == "" {
+		return fmt.Errorf("username and or password must not be empty")
+	}
+
+	retry := false
+	last := w.conf.Servers[0]
+	for _, server := range w.conf.Servers {
+		if err != nil {
+			w.dbgLog.Printf("ldap: login to server '%s' failed: %v ... trying another server", last, err)
+		}
+		retry, err = w.authenticate(server, username, password)
+		if !retry {
+			break
+		}
+		last = server
+	}
+	return err
 }
