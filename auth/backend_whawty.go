7a6c83d3cb0d84fdf74800180ddf13203788a359
Christian Pointner <equinox@spreadspace.org>
2023-11-08 22:05:51 +0100

    whawty-auth backend: implement remote upgrades

--- auth/backend_whawty.go
+++ auth/backend_whawty.go
@@ -31,20 +31,30 @@
 package auth
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 
 	"github.com/whawty/auth/store"
 )
 
+const (
+	MaxConcurrentRemoteUpgrades = 10
+)
+
 type WhawtyAuthConfig struct {
-	ConfigFile string `yaml:"store"`
+	ConfigFile       string `yaml:"store"`
+	RemoteUpgradeUrl string `yaml:"remote-upgrade-url"`
 }
 
 type WhawtyAuthBackend struct {
-	store   *store.Dir
-	infoLog *log.Logger
-	dbgLog  *log.Logger
+	store       *store.Dir
+	upgradeChan chan whawtyUpgradeRequest
+	infoLog     *log.Logger
+	dbgLog      *log.Logger
 }
 
 func NewWhawtyAuthBackend(conf *WhawtyAuthConfig, infoLog, dbgLog *log.Logger) (Backend, error) {
@@ -59,15 +69,84 @@ func NewWhawtyAuthBackend(conf *WhawtyAuthConfig, infoLog, dbgLog *log.Logger) (
 	}
 
 	b := &WhawtyAuthBackend{store: s, infoLog: infoLog, dbgLog: dbgLog}
+	if conf.RemoteUpgradeUrl != "" {
+		err = b.runRemoteUpgrader(conf.RemoteUpgradeUrl)
+		if err != nil {
+			return nil, err
+		}
+	}
 	infoLog.Printf("whawty-auth: successfully intialized store at %s (%d parameter-sets loaded)", s.BaseDir, len(s.Params))
 	return b, nil
 }
 
+type whawtyUpgradeRequest struct {
+	Session     string `json:"session,omitempty"`
+	Username    string `json:"username"`
+	OldPassword string `json:"oldpassword,omitempty"`
+	NewPassword string `json:"newpassword,omitempty"`
+}
+
+func remoteHTTPUpgrade(upgrade whawtyUpgradeRequest, remote string, infoLog, dbgLog *log.Logger) {
+	reqdata, err := json.Marshal(upgrade)
+	if err != nil {
+		infoLog.Printf("whawty-auth: error while encoding remote-upgrade request: %v", err)
+		return
+	}
+	req, _ := http.NewRequest("POST", remote, bytes.NewReader(reqdata))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		infoLog.Printf("whawty-auth: error sending remote-upgrade request: %v", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		infoLog.Printf("whawty-auth: remote-upgrade: failed for '%s' with status: %s", upgrade.Username, resp.Status)
+	} else {
+		dbgLog.Printf("whawty-auth: successfully upgraded '%s'", upgrade.Username)
+	}
+}
+
+func remoteHTTPUpgrader(upgradeChan <-chan whawtyUpgradeRequest, remote string, infoLog, dbgLog *log.Logger) {
+	sem := make(chan bool, MaxConcurrentRemoteUpgrades)
+	for upgrade := range upgradeChan {
+		select {
+		case sem <- true:
+			dbgLog.Printf("whawty-auth: upgrading '%s' via %s", upgrade.Username, remote)
+			go func(upgrade whawtyUpgradeRequest, remote string) {
+				defer func() { <-sem }()
+				remoteHTTPUpgrade(upgrade, remote, infoLog, dbgLog)
+			}(upgrade, remote)
+		default:
+			dbgLog.Printf("whawty-auth: ignoring upgrade request for '%s' due to rate-limiting", upgrade.Username)
+		}
+	}
+}
+
+func (w *WhawtyAuthBackend) runRemoteUpgrader(remote string) error {
+	r, err := url.Parse(remote)
+	if err != nil {
+		return err
+	}
+	switch r.Scheme {
+	case "http":
+		w.infoLog.Printf("whaty: using insecure url for remote upgrades: %s", remote)
+		fallthrough
+	case "https":
+		w.upgradeChan = make(chan whawtyUpgradeRequest, 10)
+		go remoteHTTPUpgrader(w.upgradeChan, remote, w.infoLog, w.dbgLog)
+	default:
+		return fmt.Errorf("whawty-auth: invalid upgrade url: %s", remote)
+	}
+	return nil
+}
+
 func (w *WhawtyAuthBackend) Authenticate(username, password string) error {
 	ok, _, upgradeable, _, err := w.store.Authenticate(username, password)
-	if upgradeable {
-		// TODO: implement remote-upgrades
-		w.dbgLog.Printf("whawty-auth: password-hash for user '%s' is upgradable, but upgrades are not implemented yet!", username)
+	if ok && upgradeable && w.upgradeChan != nil {
+		select {
+		case w.upgradeChan <- whawtyUpgradeRequest{Username: username, OldPassword: password}:
+		default: // remote upgrades are opportunistic
+		}
 	}
 	if err != nil {
 		return err
